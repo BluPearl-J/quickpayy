19c071c8028fce1870db65b3fa70027cfda7e934
ogheneokeno <ogheneokeno@gmail.com>
2026-05-13 19:02:43 +0100

    Fix: Point TestLogin to login_api and keep TestSignUp

--- quick_pay/user/tests.py
+++ quick_pay/user/tests.py
@@ -1,30 +1,24 @@
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework.test import APITestCase
 from rest_framework import status
-
 from user.models import User
-
 
 class TestSignUp(TestCase):
     def setUp(self):
         self.url_register = reverse("register")
-        self.url_login = reverse("login")
 
     def test_signup_returns_201(self):
         data = {
-        "first_name": "John",
-        "last_name": "Doe",
-        "email": "abdbasitegberongbe@gmail.com",
-        "password": "1234567",
-        "username" : "john",
-        "phone" : "08121844363",
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "abdbasitegberongbe@gmail.com",
+            "password": "1234567",
+            "username": "john",
+            "phone": "08121844363",
         }
-
         response = self.client.post(self.url_register, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
-
 
     def test_signup_returns_400(self):
         data = {
@@ -39,19 +33,20 @@ class TestSignUp(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
-
 class TestLogin(TestCase):
     def setUp(self):
         self.url_register = reverse("register")
-        self.url_login = reverse("login")
+        # Correctly pointing to the API endpoint
+        self.url_login = reverse("login_api")
         self.data = {
-                "first_name": "John",
-                "last_name": "Doe",
-                "email": "abdbasitegberongbe@gmail.com",
-                "password": "Password123",
-                "username": "john",
-                "phone": "08121844363",
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "abdbasitegberongbe@gmail.com",
+            "password": "Password123",
+            "username": "john",
+            "phone": "08121844363",
         }
+
     def test_login_returns_200(self):
         self.client.post(self.url_register, self.data, format="json")
         login_data = {
